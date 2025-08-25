115963fa7f89a73fc70fc4c1fbd0bbb6223236b1
Ivan Molanski <dalkeith@protonmail.com>
2025-08-25 22:10:30 +0000

    🚀 Deploy: Complete viral intelligence system with Firebase Functions v2, Apify integration, enterprise web GUI, and production deployment

--- web/.next/server/middleware-build-manifest.js
+++ web/.next/server/middleware-build-manifest.js
@@ -1,1 +1,19 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-ab364ee30b651afe.js","static/chunks/fd9d1056-9e191f0896c1d560.js","static/chunks/117-9b50cff5150c3fa7.js","static/chunks/main-app-d00164163bef6946.js"],pages:{"/_app":["static/chunks/webpack-ab364ee30b651afe.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-7045410c28999c29.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-ab364ee30b651afe.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-7045410c28999c29.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
