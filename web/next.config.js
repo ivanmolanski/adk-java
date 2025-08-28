6d5c5d7d62c1bcf9ebf9478ef49ba71989e6157e
Ivan Molanski <dalkeith@protonmail.com>
2025-08-28 03:54:02 +0000

    fix: Update Next.js configuration to exclude functions during build

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Exclude Firebase Functions code from Next.js build
+    config.externals = [...(config.externals || []), 'firebase-functions'];
+    
+    return config;
+  }
 };
 
 module.exports = nextConfig;
--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // Exclude Firebase Functions code from Next.js build
+    config.externals = [...(config.externals || []), 'firebase-functions'];
+    
+    return config;
+  },
+  // Explicitly set output to 'export' for static hosting
+  output: 'export'
 };
 
 export default nextConfig;
